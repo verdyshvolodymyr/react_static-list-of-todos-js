ff97bf2e6ebce6ef8097b4587a02b469130e69c9
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-04-06 13:07:59 +0300

    fixed comment for AI

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,16 +1,11 @@
 // Add the required props
-import usersFromServer from '../../api/users.json';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <article
     className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
   >
-    {usersFromServer.map(user => (
-      <>
-        <h2 className="TodoInfo__title">{todo.title}</h2>
-        <UserInfo user={user} />
-      </>
-    ))}
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    <UserInfo user={todo.user} />
   </article>
 );
